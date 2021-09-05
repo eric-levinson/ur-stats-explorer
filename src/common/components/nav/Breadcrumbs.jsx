2a57023e9e53ac4fa7b0d6b1171d4bac375e33af
Eric <eric.levinson42@gmail.com>
2021-09-04 22:17:04 -0500

    setting things up

--- src/common/components/nav/Breadcrumbs.jsx
+++ src/common/components/nav/Breadcrumbs.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,7 +20,7 @@ function handleClick(event) {
 
 export const BreadcrumbComp = (crumbs) => {
   const classes = useStyles();
-  console.log(crumbs.crumbs)
+  //console.log(crumbs)
   let links = crumbs.crumbs.map((crumb, i) => <Link color="inherit" href={crumb.link} onClick={handleClick}>{crumb.name}</Link>)
 
   return (
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,24 @@
 import logo from '../logo.svg';
 import '../App.css';
 
-import { MainNav } from '../common/components/nav/AppBar'
-import { BreadcrumbComp } from '../common/components/nav/Breadcrumbs'
+import { DrawerNav } from '../common/components/nav/Drawer'
 
 function HomePage() {
-    const crumbs = [
-        { name: 'Home', link: '/'},{ name: 'Home', link: '/'},{ name: 'Home', link: '/'}
-        ]
+    const crumbs = [{ name: 'Home', link: '/'},{ name: 'Home', link: '/'},{ name: 'Home', link: '/'}]
+    const mainNav = [{name: 'Ramen', link: '/ramen', icon: 'test-icon'},
+                    {name: 'Noodle Cup Series', link: '/ncs', icon: 'test-icon'},
+                    {name: 'Macaroni', link: '/macaroni', icon: 'test-icon'},
+                    {name: 'Fettuccine', link: '/fettuccine', icon: 'test-icon'},
+                    {name: 'Linguine', link: '/linguine', icon: 'test-icon'}]
+    const subNav = [{name: 'Core', link: '/ramen/core', icon: 'test-icon'},
+                    {name: 'Boost', link: '/ramen/boost', icon: 'test-icon'},
+                    {name: 'Positioning', link: '/ramen/positioning', icon: 'test-icon'},
+                    {name: 'Movement', link: '/ramen/movement', icon: 'test-icon'}]
     return (
         <div className="App">
-            < MainNav page='Home'/>
-            
+            < DrawerNav page='Home' crumbs={crumbs} mainnav={mainNav} subnav={subNav}/>
             <div className="App-header">
-            <BreadcrumbComp crumbs={crumbs} />
+            
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
