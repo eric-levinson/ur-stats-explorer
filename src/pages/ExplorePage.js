267c05bba336cd47cf4cdf197955a1655b61184a
Eric <eric.levinson42@gmail.com>
2021-09-14 18:23:39 -0500

    Explore things

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,35 +1,17 @@
-import logo from '../logo.svg';
-import '../App.css';
-import { useParams, useRouteMatch } from "react-router-dom";
+//import logo from '../logo.svg';
+import React from "react";
 import { ClippedDrawer } from '../common/components/nav/ClippedDrawer'
-import  BallchaseRequest  from '../utils/BallchaseRequest.js'
+
+
+//import  BallchaseRequest  from '../utils/BallchaseRequest.js'
 
 function ExplorePage(e) {
 
-    const { season, league, week, match } = useParams()
-    let { path, url } = useRouteMatch();
-    BallchaseRequest('united-rogue-d1hs10f4dh', 'group-list')
-    console.log(e)
+    //const { season, league, week, match } = useParams()
+    //BallchaseRequest('united-rogue-d1hs10f4dh', 'group-list')
     return (
-        <div className="App">
-            < ClippedDrawer />
-            <div className="App-header">
-            
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>{season} {league} {week} {match}</p>
-                <p>{path} {url} </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </div>
+        <div>
+            < ClippedDrawer/>
         </div>
     );
 }
